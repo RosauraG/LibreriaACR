33c41e907fe7986ff160c35f74ff476703a1ea10
AlondraX23 <axdamian.23@gmail.com>
2023-06-16 13:59:44 -0600

    completo

--- JS/app.js
+++ JS/app.js
@@ -1,25 +1,7 @@
-console.log("funciona")
+const $BOTON = document.getElementById("dark-mode")
+$BOTON.onclick = dark
 
-console.log(document)
-console.log(document.title)
-console.log(document.links)
-
-console.log(document.getElementsByClassName)
-console.log(document.getElementById)
-
-const $ELEMENTO = document.querySelector("a")
-
-if ($ELEMENTO.hasAttribute("href")) {
-    $ELEMENTO.removeAttribute("href")
-    console.log("Atributo eliminado")
-
-    console.log($ELEMENTO.style.backgroundColor)
-    $ELEMENTO.style.backgroundColor = "green"
-    $ELEMENTO.style.color = "white"
-    console.log($ELEMENTO.style.)
-}
-
-function dark() {
-    const $DARK = document.body
-    $DARK.getElementsByTagName("body")
+function dark () {
+    const $BODY = document.body
+    $BODY.classList.toggle("dark-theme")
 }
