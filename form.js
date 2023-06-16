10a0d482fd5f2e14ad947cbd1dd249a134764c49
carolinadamian <131982806+carolinadamian@users.noreply.github.com>
2023-06-16 13:44:00 -0600

    Reacomodo de botón y DOM

--- form.js
+++ form.js
@@ -3,7 +3,9 @@ console.log(document.getElementById("correoelectronico"))
 
 
 const $BOTON = document.getElementById("envioconfirmado")
-$BOTON.onclick = enviado
+$BOTON.onsubmit = enviado
+// onclick no permite verificar que el formulario haya sido llenado, onsubmit sí, pero la alerta de la función abajo no se muestra
+
 
 //if click on Enviar,
 //then Thank you image
@@ -19,7 +21,17 @@ $BOTON.onclick = enviado
     //$IMAGENBRILLO.style.filter = brightness(125%)
 //}
 
+
 function enviado(event){
     event.preventDefault()
+    //const $USERNAME = document.querySelector("nombreapellido").value
+    //const $EMAIL = document.querySelector("correoelectronico").value
+    //const $TEL = document.querySelector("telefono").value
+    //const $MESSAGE = document.querySelector("mensaje").value
+    //console.log("Nombre y apellido", $USERNAME)
+    //console.log("Correo", $EMAIL)
+    //console.log("Teléfono", $TEL)
+    //console.log("Mensaje", $MESSAGE)
+    //La declaración de variables y orden de mensajes anteriores no parece tener efecto sobre el mostrado de la alerta.
     alert("¡Nos pondremos en contacto contigo!")
 }
